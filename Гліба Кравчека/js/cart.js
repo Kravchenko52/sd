36277ea97d36ee437e2bde6e9b50158f58b22003
Kravchenko52 <glibk352@gmail.com>
2026-02-15 11:22:47 +0200

    Add files via upload

---
+++ Гліба Кравчека/js/cart.js
@@ -0,0 +1,22 @@
+let cart =Json.par(localStorage.getItem("cart")) || [];
+function addToCart(id,name, price, img);
+let item = cart.find(p => p.id == id)
+
+if (item){
+    item.qty +=1
+} else{
+    cart.push({
+        id,name,price,img qty:1
+    })
+     saveCart
+}
+function saveCart(){
+    localStorage.setItem("cart",JSON.stringify)
+    updateCartCount()
+}
+function updateCartCount(){
+    let count = cart.reduce((sum, item) => sum+ item.qty,0)
+    let el = document.querySelector(".cart-count"
+        if (el) el.textContent= count
+    )
+}
